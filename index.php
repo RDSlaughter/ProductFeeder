8fa5766492816d8b29ae51516057b3fdfee01c55
RDSLAUGHTER\RDSlaughter <bugra.rdesign@gmail.com>
2023-02-21 12:49:40 +0300

    Unnecessary lines deleted.

--- index.php
+++ index.php
@@ -7,34 +7,14 @@ $platformName = ucfirst(strtolower($options['p'])) ?? "Google";
 $productFileName = $options['f'] ?? "data/products.json";
 
 $productData = json_decode(file_get_contents($productFileName), true);
-
-switch ($platformName) {
-    case 'Google':
-        $feedManager = new FeedManager(new JsonFeeder());
-        break;
-    case 'Facebook':
-        $feedManager = new FeedManager(new CsvFeeder());
-        break;
-    case 'Twitter':
-        $feedManager = new FeedManager(new XmlFeeder());
-        break;
-    default:
-        die("Platform bulunamadı: $platformName!");
-}
-
 $platform = new Platform($platformName);
-
 $feeder = $platform->generateFeeder();
-
 $fileExtension = $feeder->getType();
-
 $productFeed = $feeder->getFeed($productData);
 
-$output = "./response/".$platformName.".".$fileExtension;
-
+$output = "./response/" . $platformName . "." . $fileExtension;
 file_put_contents($output, $productFeed);
-
-echo "\n".$productFeed."\nYola kaydedildi: ".$output;
+echo "\n" . $productFeed . "\nYola kaydedildi: " . $output;
 
 //$productData = json_decode(file_get_contents('data/products.json'), true);
 
